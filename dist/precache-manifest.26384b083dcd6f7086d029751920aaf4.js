7019fc36584a10101fe7709191714ec8f43fe210
Alex Ohas Emeke <ao1283@live.mdx.ac.uk>
2021-01-20 01:40:04 +0100

    save

---
+++ dist/precache-manifest.26384b083dcd6f7086d029751920aaf4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d5411d3ab13c14d507",
+    "url": "/css/app.b51c4f7f.css"
+  },
+  {
+    "revision": "331ecf46fff414458074",
+    "url": "/css/chunk-vendors.c5b0137c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9bd270033843ce510104e1d4f830e841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d5411d3ab13c14d507",
+    "url": "/js/app.862173b5.js"
+  },
+  {
+    "revision": "331ecf46fff414458074",
+    "url": "/js/chunk-vendors.fd685089.js"
+  },
+  {
+    "revision": "0de5c782ab348799d8b9957a9b64b276",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
